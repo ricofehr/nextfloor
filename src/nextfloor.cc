0fe65332fc488b34b7fcafb9385ba74142417078
ricofehr <ricofehr@nextdeploy.io>
2017-08-15 13:04:28 +0200

    clean assets

--- src/nextfloor.cc
+++ src/nextfloor.cc
@@ -18,7 +18,7 @@ int main(int argc, char* argv[])
     using nextfloor::core::ConfigEngine;
 
     /* Reset seed */
-    srand (time(NULL));
+    srand(time(NULL));
 
     /* Init Config */
     ConfigEngine::InitConfig();
