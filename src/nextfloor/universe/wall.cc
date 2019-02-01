b00f9f373a67eb4dd0be0f96a64d5352d312592b
ricofehr <ricofehr@nextdeploy.io>
2019-02-01 14:56:45 +0100

    Enable mipmap texture rendering

--- src/nextfloor/universe/wall.cc
+++ src/nextfloor/universe/wall.cc
@@ -95,7 +95,9 @@ void CreateTextureBuffer() {
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+        glGenerateMipmap(GL_TEXTURE_2D);
+
     }
 }
 
